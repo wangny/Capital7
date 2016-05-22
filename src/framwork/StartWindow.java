c35c3959cb8f0f7ddb1b8f64dc03075c8acd0844
amy <amy>
2016-05-22 20:23:52 +0800

    clear button&& start menu finish

--- src/framwork/StartWindow.java
+++ src/framwork/StartWindow.java
@@ -1,31 +1,31 @@
 package framwork;
 import controlP5.ControlP5;
 
+
+
 @SuppressWarnings("serial")
 public class StartWindow {
-	private ControlP5 cp5;
+	public ControlP5 cp5;
 	myApplet parent;
 
 	
 		
 		public StartWindow(myApplet applet)  {
 			this.parent = applet;
-		}
-		
-		public void display(){	
+			if(this.parent.gamePhase==0){
 			cp5=new ControlP5(this.parent);
 			cp5.addButton("OnePlayer")
 				.setLabel("O n e  P l a y e r")
-				.setPosition(myApplet.width-250, myApplet.height-500)
-				.setSize(200,50);
+				.setPosition(myApplet.width-525, myApplet.height-290)
+				.setSize(250,50);
 			cp5.addButton("TwoPlayer")
 				.setLabel("T w o  P l a y e r")
-				.setPosition(myApplet.width-250, myApplet.height-600)
-				.setSize(200,50);
+				.setPosition(myApplet.width-525, myApplet.height-220)
+				.setSize(250,50);
 			cp5.addButton("MultiPlayer")
-			.setLabel("M u l t i   P l a y e r")
-			.setPosition(myApplet.width-250, myApplet.height-600)
-			.setSize(200,50);
+				.setLabel("M u l t i   P l a y e r")
+				.setPosition(myApplet.width-525, myApplet.height-150)
+				.setSize(250,50);
 			
 			
 			cp5.getController("OnePlayer")
@@ -36,10 +36,16 @@ public class StartWindow {
 		       .setSize(24);
 			cp5.getController("MultiPlayer")
 		       .getCaptionLabel()
-		       .setSize(22);
+		       .setSize(24);
+			
+			}
+		}
+		
+		public void display(){	
 			
 		}
-
+		
+		
 	
 
 		
--- src/framwork/myApplet.java
+++ src/framwork/myApplet.java
@@ -24,8 +24,8 @@ public class myApplet extends PApplet{
 		startwindow = new StartWindow(this);
 		
 		currentp = new Plate(this);
-		Thread t2 = new Thread(currentp);
-		t2.start();
+		//Thread t2 = new Thread(currentp);
+		//t2.start();
 		
 		gamePhase = 0;
 	}
@@ -36,8 +36,17 @@ public class myApplet extends PApplet{
 		rect(50, 20, width-100, height-80, 15);
 		
 		if(gamePhase==0) startwindow.display();
-		else if(gamePhase==1)currentp.display();
+		else if(gamePhase==1){
+			startwindow.cp5.getController("OnePlayer").setVisible(false);
+			//startwindow.cp5.getController("OnePlayer").setValue(0);
+			startwindow.cp5.getController("TwoPlayer").setVisible(false);
+			startwindow.cp5.getController("MultiPlayer").setVisible(false);
+			this.repaint();
+			currentp.display();
+			
+		}
 	}
+	
 	
 	public void mousePressed(){
 		System.out.println("press press");
@@ -54,6 +63,14 @@ public class myApplet extends PApplet{
 	
 	public void changePhase(int phase){
 		gamePhase = phase;
+	}
+	
+	public void OnePlayer(){	
+		changePhase(1);
+		System.out.println("click one player");
+		Thread t2 = new Thread(currentp);
+		t2.start();
+		
 	}
 
 	
