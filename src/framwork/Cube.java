0c70a59edce401df6fc73d6f9f0a591d5fdfd4ed
stylelohan <stylelohan@gmail.com>
2016-05-22 10:48:13 +0800

    Revert "re-commit"
    This reverts commit b702edd9e88106dac609fc7f51f5b32d831283a4.

--- src/framwork/Cube.java
+++ src/framwork/Cube.java
@@ -10,7 +10,8 @@ public class Cube {
 	private 
 	myApplet parent;
 	public int state;
-	public int x, y;
+	public int x,y;
+	//private int colour;
 	private String name;
 	private String[] colour = {"FF004B97","FF007979","FF019858"};
 	private Map<Character,Integer> targets;
